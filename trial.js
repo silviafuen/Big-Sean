06799d0f8ae5ef7d543ff981ac1eb73ee7554ada
silviafuen <silvia.fuen@icloud.com>
2017-09-13 23:30:00 -0500

    Initial file build

---
+++ trial.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $(".intro").ready(function(){
+      $("h1").hide()
+  });
+  $(".intro").ready(function(){
+      $("h1").fadeIn(3000)
+  });
+
+    /**$(".intro").ready(function(){
+        $("h1").fadeIn(1000)
+    });
+    $(".intro").ready(function(){
+        $("h1").fadeIn();
+    });**/
+});
