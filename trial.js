8f8398d4ab6214d448d933eee0158e1112189913
silviafuen <silvia.fuen@icloud.com>
2017-09-14 20:39:42 -0500

    first content add + css for content
    All the content added to the site, images, dates, links.

--- trial.js
+++ trial.js
@@ -1,15 +1,5 @@
 $(document).ready(function(){
   $(".intro").ready(function(){
-      $("h1").hide()
-  });
-  $(".intro").ready(function(){
-      $("h1").fadeIn(3000)
-  });
-
-    /**$(".intro").ready(function(){
-        $("h1").fadeIn(1000)
+      $(".yellow-fade").fadeIn(1000);
     });
-    $(".intro").ready(function(){
-        $("h1").fadeIn();
-    });**/
 });
